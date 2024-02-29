92b66c567d51f2c6691b094d77884d7dfc32945d
Juan Fran <jfchamac@gmail.com>
2024-02-29 23:46:06 +0100

    halloween receta y recetabootstrap

---
+++ halloween/js/halloween.js
@@ -0,0 +1,21 @@
+/*****************************************************/
+/* js functions for halloween exercise               */
+/*author:                                            */
+/*date: november 2023                                */
+/*comment: my first exercise with js functions       */
+/*****************************************************/
+function show(element){ 
+    document.getElementById(element).style.visibility="visible";
+}
+
+function hide(element){
+    document.getElementById(element).style.visibility="hidden";
+}
+
+function sound(element){
+    document.getElementById(element).play()
+}
+
+function silence(element){
+    document.getElementById(element).pause()
+}
